2da7609ceb9809a9f8f329f7ebe0077e1cd797bc
luke <luke@M4M.localdomain>
2026-06-01 13:09:07 -0400

    ci: fix macOS Qt discovery and runner matrix

--- macos/build-app.sh
+++ macos/build-app.sh
@@ -29,7 +29,19 @@ fi
 export HOMEBREW_PREFIX="${HOMEBREW_PREFIX:-$(brew --prefix)}"
 export PATH="$HOMEBREW_PREFIX/bin:$HOMEBREW_PREFIX/opt/ccache/libexec:$PATH"
 
-cmake --preset "$preset"
+qt5_cmake="$HOMEBREW_PREFIX/opt/qt@5/lib/cmake"
+cmake --preset "$preset" \
+  -DQt5_DIR="$qt5_cmake/Qt5" \
+  -DQt5Core_DIR="$qt5_cmake/Qt5Core" \
+  -DQt5Widgets_DIR="$qt5_cmake/Qt5Widgets" \
+  -DQt5Gui_DIR="$qt5_cmake/Qt5Gui" \
+  -DQt5Network_DIR="$qt5_cmake/Qt5Network" \
+  -DQt5Xml_DIR="$qt5_cmake/Qt5Xml" \
+  -DQt5Sql_DIR="$qt5_cmake/Qt5Sql" \
+  -DQt5Multimedia_DIR="$qt5_cmake/Qt5Multimedia" \
+  -DQt5Script_DIR="$qt5_cmake/Qt5Script" \
+  -DQt5Svg_DIR="$qt5_cmake/Qt5Svg" \
+  -DQt5UiTools_DIR="$qt5_cmake/Qt5UiTools"
 cmake --build --preset "$preset" --parallel
 cmake --install "build/$preset"
 
