f3f9a59692062b692015015cc176405f432c972f
luke <luke@M4M.localdomain>
2026-06-06 18:27:04 -0400

    chore: disable DHT by default on macOS

--- dcpp/SettingsManager.cpp
+++ dcpp/SettingsManager.cpp
@@ -277,7 +277,7 @@ SettingsManager::SettingsManager()
     setDefault(HASH_BUFFER_PRIVATE, true);
     setDefault(RECONNECT_DELAY, 15);
     setDefault(DHT_PORT, 6250);
-    setDefault(USE_DHT, true);
+    setDefault(USE_DHT, false);
     setDefault(SEARCH_PASSIVE, false);
     setDefault(MAX_UPLOAD_SPEED_MAIN, 0);
     setDefault(MAX_DOWNLOAD_SPEED_MAIN, 0);
